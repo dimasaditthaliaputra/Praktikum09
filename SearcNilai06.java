99841b8e0b22f718d9a7b6732b0af5f7599d298d
dimasaditt <aditcraftyt@gmail.com>
2024-11-01 08:11:46 +0700

    🌏 Commit Modifikasi : Percobaan 4 Search Nilai

--- SearcNilai06.java
+++ SearcNilai06.java
@@ -2,18 +2,37 @@ import java.util.Scanner;
 
 public class SearcNilai06 {
     public static void main(String[] args) {
-        int[] arrNilai = { 80, 85, 78, 96, 90, 82, 86 };
-        int key = 90;
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Masukkan banyaknya nilai yang akan diinput: ");
+        int jmlNilai = sc.nextInt();
+        sc.nextLine();
+
+        int[] arrNilai = new int[jmlNilai];
+        int key = 0;
         int hasil = 0;
 
         for (int i = 0; i < arrNilai.length; i++) {
+            System.out.print("Masukkan nilai mahasiswa ke-" + (i + 1) + ": ");
+            arrNilai[i] = sc.nextInt();
+        }
+
+        System.out.print("Masukkan nilai yang ingin dicari: ");
+        key = sc.nextInt();
+        sc.nextLine();
+
+        for (int i = 0; i < arrNilai.length; i++) {
             if (key == arrNilai[i]) {
-                hasil = i;
+                hasil = i + 1;
                 break;
             }
         }
         System.out.println();
-        System.out.println("Nilai" + key + " ketemu di index ke-" + hasil);
+        if ( hasil != 0) {
+            System.out.println("Nilai " + key + " ketemu, merupakan nilai mahasiswa ke-" + hasil);
+        } else {
+            System.out.println("Nilai yang dicari tidak ditemukan");
+        }
         System.out.println();
     }
 }
